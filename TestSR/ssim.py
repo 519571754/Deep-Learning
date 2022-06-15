fb229bbd62fb07f516296e68e25d0097be697cc0
519571754 <35244965+519571754@users.noreply.github.com>
2022-06-15 19:24:48 +0800

    Update ssim.py

--- TestSR/ssim.py
+++ TestSR/ssim.py
@@ -1,13 +1,1 @@
-from skimage.metrics import structural_similarity as ssim
-from PIL import Image
-import numpy as np
-import cv2
-
-img1 = np.array(Image.open('00003.png'))
-img2 = np.array(Image.open('00003_out_mix_interpolation.png'))
-img2 = cv2.resize(img2, (512, 256))
-
-
-if __name__ == "__main__":
-	# If the input is a multichannel (color) image, set multichannel=True.
-    print(ssim(img1, img2, multichannel=True))
+https://blog.csdn.net/sinat_28442665/article/details/109682095
